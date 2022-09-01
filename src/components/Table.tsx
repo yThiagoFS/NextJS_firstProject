ebed7cd565b21df83dcfe7eaf91e3296b0cf341c
Thiago Ferreira <thi.ferreira.silva03@gmail.com>
2022-08-31 21:04:34 -0300

    table's style

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -7,27 +7,30 @@ const Table = (props: TableProps) => {
     function renderHeader() {
         return (
             <tr>
-                <th>Code</th>
-                <th>Name</th>
-                <th>Age</th>
+                <th className={`text-left p-4`}>Code</th>
+                <th className={`text-left p-4`}>Name</th>
+                <th className={`text-left p-4`}>Age</th>
             </tr>
         )
     }
     function renderData(){
         return props.clients?.map((client, i) => {
             return(
-                <tr key={client.id}>
-                    <td>{client.id}</td>
-                    <td>{client.name}</td>
-                    <td>{client.age}</td>
+                <tr key={client.id}
+                className={`${i % 2 === 0 ? 'bg-purple-300' : 'bg-purple-100'}`}>
+                    <td className={`text-left p-4`}>{client.id}</td>
+                    <td className={`text-left p-4`}>{client.name}</td>
+                    <td className={`text-left p-4`}>{client.age}</td>
                 </tr>
             )
         })
     }
 
     return (
-        <table>
-            <thead>
+        <table className={`w-full rounded-xl overflow-hidden`}>
+            <thead className={`
+            text-white
+            bg-gradient-to-r from-purple-500 to-purple-800`}>
                 {renderHeader()}
             </thead>
             <tbody>
